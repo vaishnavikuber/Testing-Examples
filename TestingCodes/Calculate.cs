14b7298e02725f9d6ef96533ff4b3775b95b3d12
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-26 12:02:29 +0530

    basic methods to test

---
+++ TestingCodes/Calculate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingCodes
+{
+    public class Calculate
+    {
+
+        public double area(int length, int width)
+        {
+
+            return length * width;
+        }
+
+
+        public int FactorialNum(int num)
+        {
+          
+            if (num < 0)
+                Console.WriteLine("Number must be non-negative.");
+
+            int fact = 1;
+            for (int i = num; i > 0; i--)
+            {
+                fact = fact * i;
+            }
+            return fact;
+
+        }
+
+
+        public int SumOfDigit(int n)
+        {
+            int sum = 0;
+            do
+            {
+                int d = n % 10;
+                sum = sum + d;
+                n = n / 10;
+            } while (n > 0);
+
+            return sum;
+        }
+
+
+        public int Reverse(int n)
+        {
+            int rev = 0;
+            do
+            {
+                int d = n % 10;
+                rev = rev * 10 + d;
+                n = n / 10;
+            } while (n > 0);
+
+            return rev;
+        }
+
+
+    }
+}
---
+++ TestingCodes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TestingCodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate calculate = new Calculate();
+
+            //factorial
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Factorial is " + calculate.FactorialNum(num));
+
+            //area of rectangle 
+            Console.WriteLine("Enter length");
+            int length = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter width");
+            int width = int.Parse(Console.ReadLine());
+
+            double res = calculate.area(length, width);
+            Console.WriteLine(res);
+
+            //sum of number
+            Console.WriteLine("Enter the number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            calculate.SumOfDigit(n);
+
+
+            //reverse
+            Console.WriteLine("Enter a number to reverse");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int rev = calculate.Reverse(number);
+            Console.WriteLine(rev);
+        }
+    }
+}
